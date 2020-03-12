7d4d65c7803b97322ade51e1a6299a80aedf3052
Guillaume Lours <guillaume.lours@docker.com>
2020-03-12 11:02:26 +0100

    Upgrade to Spring Boot 2.2.5
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

--- samples/spring-postgres/backend/src/main/java/com/company/project/controllers/HomeController.java
+++ samples/spring-postgres/backend/src/main/java/com/company/project/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.company.project.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
