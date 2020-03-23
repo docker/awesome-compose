75aa52524ccb9224ae6f681ddbcdcc8ae7f3e18e
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-03-23 14:18:50 +0100

    react-express-mysql: split backend code
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- react-express-mysql/backend/test/sample.js
+++ react-express-mysql/backend/test/sample.js
@@ -1,32 +1,33 @@
-const app = require('../index');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const app = require("../src/server");
 
 chai.use(chaiHttp);
 chai.should();
 
-describe('API /healthz', () => {
-    it('it should return 200', (done) => {
-        chai.request(app)
-            .get('/healthz')
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
-    });
+describe("API /healthz", () => {
+  it("it should return 200", done => {
+    chai
+      .request(app)
+      .get("/healthz")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
 
-describe('API /', () => {
-    it('it should return Welcome message', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.should.to.be.html;
-                res.text.should.be.equal("Hello Docker World\n");
-                done();
-            });
-    });
+describe("API /", () => {
+  it("it should return Welcome message", done => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.to.be.html;
+        res.text.should.be.equal("Hello Docker World\n");
+        done();
+      });
+  });
 });
-
