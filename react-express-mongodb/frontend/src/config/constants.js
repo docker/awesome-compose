e29f0d1c5429dfee004439802d6be44e39a5a9ba
Guillaume Lours <guillaume.lours@docker.com>
2020-05-12 11:18:38 +0200

    [react-express-mongdb] use compose network to communicate between services (#62)
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

--- react-express-mongodb/frontend/src/config/constants.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    API_BASE_URL: 'http://localhost:3000',
-};
-
-export default config;
--- react-express-mongodb/frontend/src/utilities/httpRequestHandler.js
+++ react-express-mongodb/frontend/src/utilities/httpRequestHandler.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import config from '../config/constants';
 
 
-export async function request (method, uri, data, headers = null, params = null) {
-    let url = (config.API_BASE_URL + uri);
+export function request (method, uri, data, headers = null, params = null) {
     let query = {
-        method: method,
-        url: url
+        method,
+        url: uri
     };
     if (headers !== null)
         query.headers = headers;
@@ -14,9 +12,5 @@ export async function request (method, uri, data, headers = null, params = null)
         query.params = params;
     if (method === 'post' || method === 'put' || method === 'delete' || method === 'patch')
         query.data = data;
-    try {
-        return await axios(query);
-    } catch (e) {
-         throw e;
-    }
+    return axios(query);
 }
