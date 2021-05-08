8119ab4025099a9ab824d8ebe3e416e05cbb0f78
Esteban Solano Granados <stvansolano@outlook.com>
2021-05-08 03:10:59 +0000

    Updated MVC app to .NET 5
    Signed-off-by: GitHub <noreply@github.com>

--- aspnet-mssql/app/aspnetapp/Program.cs
+++ aspnet-mssql/app/aspnetapp/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using aspnetapp;
+using Microsoft.Extensions.Hosting;
 
-namespace aspnetapp
+class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
