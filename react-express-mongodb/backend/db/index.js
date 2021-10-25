6505b5a53260126bd187a43c2459365958092672
Jacek Kalasz <jacek@konsternacja.pl>
2021-10-26 01:45:08 +0200

    fix mongodb unsupported attributes error Signed-off-by: Jacek Kałasz <jacek@konsternacja.pl>

--- react-express-mongodb/backend/db/index.js
+++ react-express-mongodb/backend/db/index.js
@@ -7,11 +7,6 @@ exports.connect = (app) => {
   const options = {
     useNewUrlParser: true,
     autoIndex: false, // Don't build indexes
-    reconnectTries: 30, // Retry up to 30 times
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
   };
 
   const connectWithRetry = () => {
