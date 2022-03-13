d128c1e568d20e52c82068e1e0abc6467df25287
Ajeet Singh Raina, Docker Captain, ARM Innovator, RedisInc <ajeetraina@gmail.com>
2022-03-13 16:47:58 +0530

    Update nginx-nodejs-redis/web/server.js
    Co-authored-by: Guillaume Lours <guillaume@lours.me>

--- nginx-nodejs-redis/web/server.js
+++ nginx-nodejs-redis/web/server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
         }
-        res.send('Number of visits is: ' + numVisitsToDisplay);
+       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
         numVisits++;
         redisClient.set('numVisits', numVisits);
     });
