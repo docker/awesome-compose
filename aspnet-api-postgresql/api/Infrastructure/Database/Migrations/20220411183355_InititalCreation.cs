4eef6ec88c5c257abdb2625dc1bdf437062899fe
Eduardo Silva <eduardo.lour.silva@gmail.com[core]>
2022-04-11 16:33:57 -0300

    create aspnet api with postgresql
    This version added a compose application using:
    * ASP.Net API Core
    * EntityFramework Core
    * Postgresql
    
    Signed-off-by: Eduardo Silva <eduardo.lour.silva@gmail.com>

---
+++ aspnet-api-postgresql/api/Infrastructure/Database/Migrations/20220411183355_InititalCreation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnet_api_postgresql.Infrastructure.Database.Migrations
+{
+    public partial class InititalCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
