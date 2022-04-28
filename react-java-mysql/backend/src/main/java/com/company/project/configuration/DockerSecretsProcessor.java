3b79604af297832c12d181e976fb7d778016afb7
Amin Al-Ali Al-Darwish <aminalali8@gmail.com>
2022-04-28 14:34:58 +0100

    Update DockerSecretsProcessor.java

--- react-java-mysql/backend/src/main/java/com/company/project/configuration/DockerSecretsProcessor.java
+++ react-java-mysql/backend/src/main/java/com/company/project/configuration/DockerSecretsProcessor.java
@@ -17,14 +17,15 @@ public class DockerSecretsProcessor implements EnvironmentPostProcessor {
 
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-		Resource resource = new FileSystemResource("/run/secrets/db-password");
-		if (resource.exists() && System.getProperty("MYSQL_PASSWORD") == null) {
-			try {
-				String dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
-				System.setProperty("MYSQL_PASSWORD", dbPassword);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
+// 		Resource resource = new FileSystemResource("/run/secrets/db-password");
+// 		if (resource.exists() && System.getProperty("MYSQL_PASSWORD") == null) {
+// 			try {
+// 				String dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+// 				System.setProperty("MYSQL_PASSWORD", dbPassword);
+// 			} catch (IOException e) {
+// 				throw new RuntimeException(e);
+// 			}
+// 		}
+		System.setProperty("MYSQL_PASSWORD", "db-57xsl");
 	}
 }
