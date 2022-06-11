718df2a62db54f6aa75660f3f35b2e098845ef1c
Esteban Solano Granados <stvansolano@outlook.com>
2022-06-11 01:26:09 +0000

    Added backend
    Signed-off-by: GitHub <noreply@github.com>

---
+++ react-aspnet-mongodb/backend/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = File.ReadAllText("/run/secrets/db-connection");
+builder.Services.AddTransient<MongoClient>((_provider) => new MongoClient(connectionString));
+
+var app = builder.Build();
+
+app.MapGet("/api", async (MongoClient connection) => {
+    try
+    {
+        var client = new MongoClient(connectionString);  
+        var database = client.GetDatabase("BackendMongoDb");  
+
+        var collection = database.GetCollection<ToDo>("ToDos");
+        var results = await collection.Find(_ => true).ToListAsync().ConfigureAwait(false);
+
+        return Results.Ok(results);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(detail: ex.ToString());
+    }
+});
+
+app.Run();
