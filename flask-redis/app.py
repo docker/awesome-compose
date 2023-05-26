51ccb0f692ce47b7cf6c113bb104890cb75cad7e
Sourcery AI <>
2023-05-26 13:28:08 +0000

    'Refactored by Sourcery'

--- flask-redis/app.py
+++ flask-redis/app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 def hello():
     redis.incr('hits')
     counter = str(redis.get('hits'),'utf-8')
-    return "This webpage has been viewed "+counter+" time(s)"
+    return f"This webpage has been viewed {counter} time(s)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
