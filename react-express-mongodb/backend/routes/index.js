d8170dcd7b64bf60987ef96f5bb608e2ff0d38c7
Joseph Angelo Ferrer Barrozo <jabarrozo@gmail.com>
2024-02-21 10:16:49 +0800

    extracted backend methods into their own controller
    Signed-off-by: Joseph Angelo Ferrer Barrozo <jabarrozo@gmail.com>

--- react-express-mongodb/backend/routes/index.js
+++ react-express-mongodb/backend/routes/index.js
@@ -1,35 +1,12 @@
 const express = require("express");
-const serverResponses = require("../utils/helpers/responses");
-const messages = require("../config/messages");
-const { Todo } = require("../models/todos/todo");
+const todoController = require("../controllers/todos/todoController");
 
 const routes = (app) => {
   const router = express.Router();
 
-  router.post("/todos", (req, res) => {
-    const todo = new Todo({
-      text: req.body.text,
-    });
+  router.post("/todos", todoController.createTodo);
 
-    todo
-      .save()
-      .then((result) => {
-        serverResponses.sendSuccess(res, messages.SUCCESSFUL, result);
-      })
-      .catch((e) => {
-        serverResponses.sendError(res, messages.BAD_REQUEST, e);
-      });
-  });
-
-  router.get("/", (req, res) => {
-    Todo.find({}, { __v: 0 })
-      .then((todos) => {
-        serverResponses.sendSuccess(res, messages.SUCCESSFUL, todos);
-      })
-      .catch((e) => {
-        serverResponses.sendError(res, messages.BAD_REQUEST, e);
-      });
-  });
+  router.get("/", todoController.getAllTodos);
 
   //it's a prefix before api it is useful when you have many modules and you want to
   //differentiate b/w each module you can use this technique
