6a3211cc452a23370f4e04c690afdd5cff54c864
siranosnut3407 <arishtunson@gmail.com>
2024-06-18 11:38:43 -0700

    remove references to Dev Environments feature Signed-off-by: Guillaume Lours <705411+glours@users.noreply.github.com>

--- nginx-aspnet-mysql/backend/Program.cs
+++ nginx-aspnet-mysql/backend/Program.cs
@@ -1,62 +1,69 @@
 ﻿using MySqlConnector;
 
-var builder = WebApplication.CreateBuilder(args);
-
-string password = File.ReadAllText("/run/secrets/db-password");
-string connectionString = $"server=db;user=root;database=example;port=3306;password={password}";
-
-builder.Services.AddTransient<MySqlConnection>((_provider) => new MySqlConnection(connectionString));
-
-var app = builder.Build();
-
-app.MapGet("/", (MySqlConnection connection) => {
-    var titles = new List<string>();
-
-    try
-    {
-        Console.WriteLine("Connecting to MySQL...");
-        connection.Open();
-
-        string sql = "SELECT title FROM blog";
-        using var cmd = new MySqlCommand(sql, connection);
-        using MySqlDataReader reader = cmd.ExecuteReader();
-
-        while (reader.Read())
-        {
-            titles.Add(reader.GetString(0));
-        }
-        reader.Close();
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(detail: ex.ToString());
-    }
-    connection.Close();
-    
-    return Results.Ok(titles);
-});
-Prepare(connectionString);
-
-app.Run();
-
-void Prepare(string connectionString)
+internal class Program
 {
-    using MySqlConnection connection = new MySqlConnection(connectionString);
-
-    connection.Open();
-    using var transation = connection.BeginTransaction();
-
-    using MySqlCommand cmd1 = new MySqlCommand("DROP TABLE IF EXISTS blog", connection, transation);
-    cmd1.ExecuteNonQuery();
-
-    using MySqlCommand cmd2 = new MySqlCommand("CREATE TABLE IF NOT EXISTS blog (id int NOT NULL AUTO_INCREMENT, title varchar(255), PRIMARY KEY (id))", connection, transation);
-    cmd2.ExecuteNonQuery();
-    
-    for (int i = 0; i < 5; i++)
+    private static void Main(string[] args)
     {
-        using MySqlCommand insertCommand = new MySqlCommand( $"INSERT INTO blog (title) VALUES ('Blog post #{i}');", connection, transation);
-        insertCommand.ExecuteNonQuery();
+        var builder = WebApplication.CreateBuilder(args);
+
+        string password = File.ReadAllText("/run/secrets/db-password");
+        string connectionString = $"server=db;user=root;database=example;port=3306;password={password}";
+
+        builder.Services.AddTransient<MySqlConnection>((_provider) => new MySqlConnection(connectionString));
+
+        var app = builder.Build();
+
+        app.MapGet("/", (MySqlConnection connection) =>
+        {
+            var titles = new List<string>();
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                connection.Open();
+
+                string sql = "SELECT title FROM blog";
+                using var cmd = new MySqlCommand(sql, connection);
+                using MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    titles.Add(reader.GetString(0));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(detail: ex.ToString());
+            }
+            connection.Close();
+
+            return Results.Ok(titles);
+        });
+        Prepare(connectionString);
+
+        app.Run();
+
+        void Prepare(string connectionString)
+        {
+            using MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+            using var transation = connection.BeginTransaction();
+
+            using MySqlCommand cmd1 = new MySqlCommand("DROP TABLE IF EXISTS blog", connection, transation);
+            cmd1.ExecuteNonQuery();
+
+            using MySqlCommand cmd2 = new MySqlCommand("CREATE TABLE IF NOT EXISTS blog (id int NOT NULL AUTO_INCREMENT, title varchar(255), PRIMARY KEY (id))", connection, transation);
+            cmd2.ExecuteNonQuery();
+
+            for (int i = 0; i < 5; i++)
+            {
+                using MySqlCommand insertCommand = new MySqlCommand($"INSERT INTO blog (title) VALUES ('Blog post #{i}');", connection, transation);
+                insertCommand.ExecuteNonQuery();
+            }
+            transation.Commit();
+            connection.Close();
+        }
     }
-    transation.Commit();
-    connection.Close();
 }
